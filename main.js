752be7b8291842b2434a20570f7c982aceeb7742
Trey Simmons <yert511@gmail.com>
2022-03-01 12:53:45 -0600

    Update script to remove broken scroll and fix date counter

--- main.js
+++ main.js
@@ -1,22 +1,8 @@
-function setupSmoothScrolling() {
-   $('a[href*="#"]:not([href="#"]):not([href$="modal"])').click(function() {
-     var target = $(this.hash);
-     if (target.length) {
-       $("html, body").animate(
-         {scrollTop: target.offset().top},
-         800
-       );
-     }
-   });
-}
-
+'use strict';
 function setYearsCounter(startDate) {
-  var today = new Date();
-  var yearsDiff = (Math.floor((today-startDate)/31556952000));
-  $('.years-counter').text(yearsDiff);
+    let today = new Date();
+    let yearsDiff = (Math.floor((today-startDate)/31556952000));
+    document.getElementById('years-counter').textContent = yearsDiff + " years";
 }
 
-$(function() {
-  setupSmoothScrolling();
-  setYearsCounter(new Date('2013','09','15'));
-});
+setYearsCounter(new Date('2013','09','15'));
